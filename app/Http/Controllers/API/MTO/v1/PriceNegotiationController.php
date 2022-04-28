bb7dd0998b24e441f473f7da6b14a0978a787587
vladislav <vladislav@bik-agency.ru>
2022-04-28 17:06:02 +0300

    added queue price negotiation

--- app/Http/Controllers/API/MTO/v1/PriceNegotiationController.php
+++ app/Http/Controllers/API/MTO/v1/PriceNegotiationController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Models\PriceNegotiations\PriceNegotiation;
 use App\Serializers\CustomerSerializer;
 use App\Transformers\API\MTO\v1\PriceNegotiationTransformer;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -25,6 +26,27 @@ class PriceNegotiationController extends Controller
                     ->get();
 //                PriceNegotiation::query()->whereIn('id', $orders->pluck('id'))->update(['sync_required' => false]);#todo: расскомментировать в будущем
                 return fractal()->collection($price_negotiations)->transformWith(PriceNegotiationTransformer::class)->serializeWith(CustomerSerializer::class);
+            });
+        } catch (\Exception $e) {
+            Log::error($e->getMessage(), $e->getTrace());
+            return response()->json(['message' => 'System error'], 500);
+        }
+    }
+
+    public function putInQueue(Request $request)
+    {
+        $request->validate([
+            'ids' => 'required|array',
+            'ids.*' => 'required|uuid',
+        ]);
+        try {
+            return DB::transaction(function () use ($request) {
+                $count = PriceNegotiation::withoutEvents(function () use ($request) {
+                    return PriceNegotiation::query()
+                        ->whereIn('uuid', $request->ids)
+                        ->update(['sync_required' => true]);
+                });
+                return response()->json('В очередь поставлено ' . $count . ' сущностей по согласованию цен');
             });
         } catch (\Exception $e) {
             Log::error($e->getMessage(), $e->getTrace());
