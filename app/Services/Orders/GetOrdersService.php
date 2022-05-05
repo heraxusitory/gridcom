cc62514fa5434fa5caba64f32dbfa86099b99e76
vladislav <vladislav@bik-agency.ru>
2022-05-05 16:45:56 +0300

    fix orders webapi

--- app/Services/Orders/GetOrdersService.php
+++ app/Services/Orders/GetOrdersService.php
@@ -24,11 +24,11 @@ class GetOrdersService implements IService
     public function run()
     {
         $orders = Order::query()
-            ->with(['customer.contract', 'provider.contract', 'contractor'])
-            ->whereRelation('customer', 'customer_status', '<>', Order::CUSTOMER_STATUS_DRAFT)
-            ->whereRelation('provider', 'provider_status', '<>', Order::PROVIDER_STATUS_DRAFT);
+            ->with(['customer.contract', 'provider.contract', 'contractor']);
         if ($this->user->isProvider()) {
-            $orders->whereRelation('provider', 'contr_agent_id', $this->user->contr_agent_id());
+            $orders->whereRelation('provider', 'contr_agent_id', $this->user->contr_agent_id())
+                ->whereRelation('customer', 'customer_status', '<>', Order::CUSTOMER_STATUS_DRAFT)
+                ->whereRelation('provider', 'provider_status', '<>', Order::PROVIDER_STATUS_DRAFT);
         } elseif ($this->user->isContractor()) {
             $orders->whereRelation('contractor', 'contr_agent_id', $this->user->contr_agent_id());
         }
