adbf67e0d28aa6205e2e0256cb0a5e10ff423320
vladislav <vladislav@bik-agency.ru>
2022-05-17 15:28:12 +0300

    fix migration orders

---
+++ database/migrations/2022_05_17_122329_change_statuses_in_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeStatusesInOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            DB::statement('ALTER TABLE orders DROP CONSTRAINT orders_customer_status_check');
+            DB::statement('ALTER TABLE orders DROP CONSTRAINT orders_provider_status_check');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
