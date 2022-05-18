199eb9357dcf2918d59e9f9a869f7e14998512a3
vladislav <vladislav@bik-agency.ru>
2022-05-18 15:01:23 +0300

    fix

--- app/Transformers/API/MTO/v1/PaymentRegisterPositionTransformer.php
+++ app/Transformers/API/MTO/v1/PaymentRegisterPositionTransformer.php
@@ -34,14 +34,14 @@ class PaymentRegisterPositionTransformer extends TransformerAbstract
     {
         return [
             'position_id' => $payment_register_position?->position_id,
-            'order_id' => optional($payment_register_position->order)->uuid,
+            'order_id' => optional($payment_register_position?->order)->uuid,
             'payment_order_date' => $payment_register_position?->payment_order_date,
             'payment_order_number' => $payment_register_position?->payment_order_number,
             'amount_payment' => $payment_register_position?->amount_payment,
             'payment_type' => $payment_register_position?->payment_type,
-            'object_id' => optional($payment_register_position->order->customer->object)->uuid,
-            'organization_id' => optional($payment_register_position->order->customer->organization)->uuid,
-            'work_agreement_id' => optional($payment_register_position->order->customer->contract)->uuid,
+            'object_id' => optional($payment_register_position?->order?->customer?->object)->uuid,
+            'organization_id' => optional($payment_register_position?->order?->customer?->organization)->uuid,
+            'work_agreement_id' => optional($payment_register_position?->order?->customer?->contract)->uuid,
         ];
     }
 }
