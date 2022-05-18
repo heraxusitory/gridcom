b2df5a45554a894b38aaa58b0a7faad862c1ee40
vladislav <vladislav@bik-agency.ru>
2022-05-18 12:49:40 +0300

    fix stack listener

--- app/Listeners/StackListener.php
+++ app/Listeners/StackListener.php
@@ -3,10 +3,12 @@
 namespace App\Listeners;
 
 use App\Events\NewStack;
+use App\Models\IntegrationUser;
 use App\Models\Orders\Order;
 use App\Models\SyncStacks\MTOSyncStack;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
@@ -36,8 +38,12 @@ class StackListener
         foreach ($stacks as $stack) {
             $stack->model = $model_object::class;
             $stack->entity_id = $model_object->id;
-            if (!($stack instanceof MTOSyncStack) && !Str::isUuid($stack->contr_agent_id))
+            if (!($stack instanceof MTOSyncStack) && !Str::isUuid($stack->contr_agent_id) &&
+                $user = IntegrationUser::query()
+                    ->whereRelation('contr_agent', 'uuid', $stack->contr_agent_id)
+                    ->doesntExist()) {
                 continue;
+            }
             $stack->save();
         }
     }
