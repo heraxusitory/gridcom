1df2343a041acd716f8115030fbc2770e3bdec6a
vladislav <vladislav@bik-agency.ru>
2022-05-19 15:57:10 +0300

    fix

--- routes/api/lkk-mto/v1/consignment_registers.php
+++ routes/api/lkk-mto/v1/consignment_registers.php
@@ -4,7 +4,7 @@
 use App\Http\Controllers\API\MTO\v1\ConsignmentRegisterController;
 
 Route::group(['prefix' => 'consignment_registers'], function () {
-//    Route::post('sync', [ConsignmentRegisterController::class, 'sync']);
+    Route::post('sync', [ConsignmentRegisterController::class, 'sync']);
     Route::post('synchronize', [ConsignmentRegisterController::class, 'synchronize']);
     Route::post('remove_from_stack', [ConsignmentRegisterController::class, 'removeFromStack']);
 
