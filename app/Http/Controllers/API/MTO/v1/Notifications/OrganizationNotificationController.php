29527c4cf068b3841e552f469de4d25fac4c45a0
vladislav <vladislav@bik-agency.ru>
2022-06-01 14:48:02 +0300

    fix

--- app/Http/Controllers/API/MTO/v1/Notifications/OrganizationNotificationController.php
+++ app/Http/Controllers/API/MTO/v1/Notifications/OrganizationNotificationController.php
@@ -4,9 +4,11 @@
 namespace App\Http\Controllers\API\MTO\v1\Notifications;
 
 
+use App\Events\NewStack;
 use App\Http\Controllers\Controller;
 use App\Models\Notifications\OrganizationNotification;
 use App\Models\SyncStacks\MTOSyncStack;
+use App\Models\SyncStacks\ProviderSyncStack;
 use App\Serializers\CustomerSerializer;
 use App\Transformers\API\MTO\v1\OrganizationNotificationTransformer;
 use Illuminate\Http\Request;
@@ -26,7 +28,14 @@ class OrganizationNotificationController extends Controller
         ]);
         try {
             foreach ($request['organization_notifications'] as $notification) {
-                OrganizationNotification::query()->where('uuid', $notification['id'])->update(['status' => $notification['status']]);
+                /** @var OrganizationNotification $organization_notification */
+                $organization_notification = OrganizationNotification::query()->where('uuid', $notification['id'])->first();
+                if ($organization_notification) {
+                    $organization_notification = $organization_notification->save(['status' => $notification['status']]);
+                    event(new NewStack($organization_notification,
+                        (new ProviderSyncStack())->setProvider($organization_notification->provider),
+                    ));
+                }
             }
             return response()->json();
         } catch (\Exception $e) {
