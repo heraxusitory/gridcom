caa7dd53c4a8ecffc38a557ed0641be087fe7f14
vladislav <vladislav@bik-agency.ru>
2022-06-24 11:17:00 +0300

    fix

--- app/Http/Controllers/WebAPI/v1/Consignments/ConsignmentController.php
+++ app/Http/Controllers/WebAPI/v1/Consignments/ConsignmentController.php
@@ -44,9 +44,9 @@ class ConsignmentController extends Controller
         try {
             $consignments = (new GetConsignmentsService($request, $filter, $sorting))->run();
             return fractal()->collection($consignments)->transformWith(ConsignmentTransformer::class)
-                /*->serializeWith(new ArraySerializer())
-                ->paginateWith(new IlluminatePaginatorAdapter($consignments))
-                ->toArray()*//*->serializeWith(CustomerSerializer::class)*/ ;
+//                ->serializeWith(new ArraySerializer())
+                ->paginateWith(new IlluminatePaginatorAdapter($consignments));
+//                ->toArray()*//*->serializeWith(CustomerSerializer::class)*/ ;
 //            return response()->json($consignments);
         } catch (\Exception $e) {
             if ($e->getCode() >= 400 && $e->getCode() < 500)
